a433838e7048fae3cc2277d681d4de516a26f962
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-02-27 00:15:27 +0200

    added library with functions, functions reduced to one, added functions in dom

--- domOperations.js
+++ domOperations.js
@@ -1,1 +1,47 @@
+    
+const WRONG_INPUT_MESSAGE = "Неверный ввод";
 
+function isNonNegativeNumber(side) {
+    return !isNaN(side) && side >= 0
+}
+
+function showResultInDomKvadrat(){
+    const squareSide = document.getElementById("dlinaStoroniKvadrata").value;
+
+    document.getElementById("ploschadKvadrata")
+            .textContent = isNonNegativeNumber(squareSide)
+            ? multiplyTwoValues(squareSide, squareSide).toFixed(2)
+            : WRONG_INPUT_MESSAGE;
+}
+
+function showResultInDomPriamougolnika() {
+    const rectangularSideA = document.getElementById("pervayaStoronaPriamougolnika").value;
+    const rectangularSideB = document.getElementById("drugayaStoronaPriamougolnika").value;
+
+    document.getElementById("ploschadPriamougolnika")
+            .textContent = isNonNegativeNumber(rectangularSideA) && isNonNegativeNumber(rectangularSideB)
+            ? multiplyTwoValues(rectangularSideA, rectangularSideB).toFixed(2)
+            : WRONG_INPUT_MESSAGE;
+
+}
+
+function showResultInDomParalelograma() {
+    const parallelogramSide = document.getElementById("dlinaStoronyParalelograma").value;
+    const parallelogramHeight = document.getElementById("dlinaVisotyParalelograma").value;
+
+    document.getElementById("ploschadParalelograma")
+            .textContent = isNonNegativeNumber(parallelogramSide) && isNonNegativeNumber(parallelogramHeight)
+            ? multiplyTwoValues(parallelogramSide, parallelogramHeight).toFixed(2)
+            : WRONG_INPUT_MESSAGE;
+}
+
+function showResultInDomRomba() {
+    const diamondSide = document.getElementById("dlinaStoronyRomba").value;
+    const diamondHeight = document.getElementById("dlinaVisotyRomba").value;
+
+    document.getElementById("ploschadRomba")
+            .textContent = isNonNegativeNumber(diamondSide) && isNonNegativeNumber(diamondHeight)
+            ? multiplyTwoValues(diamondSide, diamondHeight).toFixed(2)
+            : WRONG_INPUT_MESSAGE;
+
+}
--- libraryFormula.js
+++ libraryFormula.js
@@ -1,1 +1,22 @@
+function multiplyTwoValues(firstValue, secondValue){
+    return firstValue * secondValue;
+}
 
+
+/*
+Intentionally commented functions
+Instead of 3 or 4 functions decided to create 1 which could be a good practice
+to follow DRY principle and not duplicate the similar calculations.
+
+function squareArea (a){
+    return a * a;
+}
+
+function rectangularArea(a, b){
+    return a * b;
+}
+
+function parallelogramOrDiamondArea(a, h){
+    return a * h;
+} 
+*/
