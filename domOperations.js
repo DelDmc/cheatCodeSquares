d8510bdd55e0c75950e510b91f64f97f299332ca
K.S <del12dmc@gmail.com>
2023-05-16 00:43:46 +0300

    correct as per linter

--- domOperations.js
+++ domOperations.js
@@ -1,39 +1,72 @@
-    
-const WRONG_INPUT_MESSAGE = "Неверный ввод";
 
-function isNonNegativeNumber(side) {
-    return !isNaN(side) && side >= 0;
+/**
+ * Error message for wrong input.
+ * @constant {string}
+ */
+const WRONG_INPUT_MESSAGE = 'Неверный ввод'
+
+/**
+ * Checks if a given side is a non-negative number.
+ * @param {number} side - The value of the side to check.
+ * @returns {boolean} - Indicates whether the side is a non-negative number.
+ */
+function isNonNegativeNumber (side) {
+  return !isNaN(side) && side >= 0
 }
 
+/**
+ * Calculates the area of a shape and updates the specified DOM element with the result.
+ * @param {HTMLElement} element - The DOM element to display the calculated area.
+ * @param {number} sideA - The value of the first side of the shape.
+ * @param {number} sideB - The value of the second side of the shape.
+ * @param {Function} areaFunc - The function to calculate the area of the shape.
+ * @returns {void}
+ */
 function calculateShapeArea (element, sideA, sideB, areaFunc) {
-            element.textContent = isNonNegativeNumber(sideA) && isNonNegativeNumber(sideB)?  
-                    areaFunc(sideA, sideB).toFixed(2): 
-                    WRONG_INPUT_MESSAGE;
+  element.textContent = isNonNegativeNumber(sideA) && isNonNegativeNumber(sideB)
+    ? areaFunc(sideA, sideB).toFixed(2)
+    : WRONG_INPUT_MESSAGE
 }
 
-function showResultInDomKvadrat(){
-    const squareAreaElem = document.getElementById("ploschadKvadrata");
-    const squareSide = document.getElementById("dlinaStoroniKvadrata").value;
-    calculateShapeArea(squareAreaElem, squareSide, squareSide, calculateSquareArea);
+/**
+ * Calculates and displays the area of a square in the DOM.
+ * @returns {void}
+ */
+function showResultInDomKvadrat () {
+  const squareAreaElem = document.getElementById('ploschadKvadrata')
+  const squareSide = document.getElementById('dlinaStoroniKvadrata').value
+  calculateShapeArea(squareAreaElem, squareSide, squareSide, calculateSquareArea)
 }
 
-function showResultInDomPriamougolnika() {
-    const rectangularAreaElem = document.getElementById("ploschadPriamougolnika");
-    const rectangularSideA = document.getElementById("pervayaStoronaPriamougolnika").value;
-    const rectangularSideB = document.getElementById("drugayaStoronaPriamougolnika").value;
-    calculateShapeArea(rectangularAreaElem, rectangularSideA, rectangularSideB, calculateRectangularArea);
+/**
+ * Calculates and displays the area of a rectangle in the DOM.
+ * @returns {void}
+ */
+function showResultInDomPriamougolnika () {
+  const rectangularAreaElem = document.getElementById('ploschadPriamougolnika')
+  const rectangularSideA = document.getElementById('pervayaStoronaPriamougolnika').value
+  const rectangularSideB = document.getElementById('drugayaStoronaPriamougolnika').value
+  calculateShapeArea(rectangularAreaElem, rectangularSideA, rectangularSideB, calculateRectangularArea)
 }
 
-function showResultInDomParalelograma() {
-    const parallelogramAreaElem = document.getElementById("ploschadParalelograma");
-    const parallelogramSide = document.getElementById("dlinaStoronyParalelograma").value;
-    const parallelogramHeight = document.getElementById("dlinaVisotyParalelograma").value;
-    calculateShapeArea(parallelogramAreaElem, parallelogramSide, parallelogramHeight, calculateParallelogramArea);
+/**
+ * Calculates and displays the area of a parallelogram in the DOM.
+ * @returns {void}
+ */
+function showResultInDomParalelograma () {
+  const parallelogramAreaElem = document.getElementById('ploschadParalelograma')
+  const parallelogramSide = document.getElementById('dlinaStoronyParalelograma').value
+  const parallelogramHeight = document.getElementById('dlinaVisotyParalelograma').value
+  calculateShapeArea(parallelogramAreaElem, parallelogramSide, parallelogramHeight, calculateParallelogramArea)
 }
 
-function showResultInDomRomba() {
-    const diamondAreaElem = document.getElementById("ploschadRomba");
-    const diamondSide = document.getElementById("dlinaStoronyRomba").value;
-    const diamondHeight = document.getElementById("dlinaVisotyRomba").value;
-    calculateShapeArea(diamondAreaElem, diamondSide, diamondHeight, calculateDiamondArea);
+/**
+ * Calculates and displays the area of a diamond in the DOM.
+ * @returns {void}
+ */
+function showResultInDomRomba () {
+  const diamondAreaElem = document.getElementById('ploschadRomba')
+  const diamondSide = document.getElementById('dlinaStoronyRomba').value
+  const diamondHeight = document.getElementById('dlinaVisotyRomba').value
+  calculateShapeArea(diamondAreaElem, diamondSide, diamondHeight, calculateDiamondArea)
 }
--- libraryFormula.js
+++ libraryFormula.js
@@ -1,19 +1,48 @@
-function multiplyTwoSides(sideA, sideB){
-    return sideA * sideB;
+/**
+ * Calculates the area of a square.
+ * @param {number} side - The length of the side of the square.
+ * @returns {number} - The area of the square.
+ */
+function calculateSquareArea (side) {
+  return multiplyTwoSides(side, side)
 }
 
-function calculateSquareArea (side){
-    return multiplyTwoSides(side, side);
+/**
+   * Calculates the area of a rectangle.
+   * @param {number} sideA - The length of one side of the rectangle.
+   * @param {number} sideB - The length of the other side of the rectangle.
+   * @returns {number} - The area of the rectangle.
+   */
+function calculateRectangularArea (sideA, sideB) {
+  return multiplyTwoSides(sideA, sideB)
 }
 
-function calculateRectangularArea(sideA, sideB){
-    return multiplyTwoSides(sideA, sideB);
+/**
+   * Calculates the area of a parallelogram.
+   * @param {number} sideA - The length of the base of the parallelogram.
+   * @param {number} height - The height of the parallelogram.
+   * @returns {number} - The area of the parallelogram.
+   */
+function calculateParallelogramArea (sideA, height) {
+  return multiplyTwoSides(sideA, height)
 }
 
-function calculateParallelogramArea(sideA, height){
-    return multiplyTwoSides(sideA, height);
-} 
+/**
+   * Calculates the area of a diamond.
+   * @param {number} sideA - The length of one side of the diamond.
+   * @param {number} height - The height of the diamond.
+   * @returns {number} - The area of the diamond.
+   */
+function calculateDiamondArea (sideA, height) {
+  return multiplyTwoSides(sideA, height)
+}
 
-function calculateDiamondArea(sideA, height){
-    return multiplyTwoSides(sideA, height);
+/**
+   * Multiplies two sides to calculate the area of a shape.
+   * @param {number} sideA - The length of one side.
+   * @param {number} sideB - The length of the other side.
+   * @returns {number} - The calculated area.
+   */
+function multiplyTwoSides (sideA, sideB) {
+  return sideA * sideB
 }
